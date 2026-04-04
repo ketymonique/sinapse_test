1ae89355eb7c0c0e8c5817d496b93702769369f7
Kethelyn Couto <kety_couto@hotmail.com>
2026-04-04 09:26:13 -0300

    Implementando a ISO 8601

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use app\Rules\StrongPassword;
 
 class UpdateUserRequest extends FormRequest
 {
